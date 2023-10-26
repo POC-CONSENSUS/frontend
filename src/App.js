0aeba2f249ce53b00d4a6c943cb67c4c25c4ca1a
Sylvain Martin <symartin@redhat.com>
2023-10-26 23:20:43 +0200

    Draft form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import ProductPicker from './component/Product';
 
 import './App.css';
 
 function App() {
 
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(false);
 
   useEffect(() => {
     fetch('https://consensus-user1-apicast-production.apps.cluster-sglv8.sglv8.sandbox2890.opentlc.com:443/?user_key=2a8d9328738a099871b0b5919b4f175d')
@@ -13,11 +14,21 @@ function App() {
       .catch(error => console.error(error));
   }, []);
 
-
-  return (
+  return data && (
     <div className="App">
-      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
-    </div>
+      <h1>Consensus naming convention helper form</h1>
+      <form action="" method="get" class="form-example">
+        <div class="form-example">
+          <ProductPicker products={data.products} uniqueId="1" label="Select first product" />
+        </div>
+        <div class="form-example">
+          <ProductPicker products={data.products} uniqueId="2" label="(OPTIONAL) Select second product" />
+        </div>
+        <div class="form-example">
+          <ProductPicker products={data.products} uniqueId="3" label="(OPTIONAL) Select third product" />
+        </div>
+      </form>
+    </div >
   );
 }
 
